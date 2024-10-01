2947b938a4b310fd6fd7e455525501ba8492844f
Semonzz <151677299+Semonzz@users.noreply.github.com>
2024-10-01 13:39:46 +0300

    lab2

---
+++ 2/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿using System.Windows.Forms;
+
+namespace lab2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.showMatrixButton = new System.Windows.Forms.Button();
+            this.inputPanel = new System.Windows.Forms.Panel();
+            this.outputPanel = new System.Windows.Forms.Panel();
+            this.widthTrackBar = new System.Windows.Forms.TrackBar();
+            this.heightTrackBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelError = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.widthTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // showMatrixButton
+            // 
+            this.showMatrixButton.Location = new System.Drawing.Point(349, 165);
+            this.showMatrixButton.Name = "showMatrixButton";
+            this.showMatrixButton.Size = new System.Drawing.Size(115, 72);
+            this.showMatrixButton.TabIndex = 0;
+            this.showMatrixButton.Text = "Показать матрицу";
+            this.showMatrixButton.UseVisualStyleBackColor = true;
+            this.showMatrixButton.Click += new System.EventHandler(this.showMatrixButton_Click);
+            // 
+            // inputPanel
+            // 
+            this.inputPanel.Location = new System.Drawing.Point(12, 68);
+            this.inputPanel.Name = "inputPanel";
+            this.inputPanel.Size = new System.Drawing.Size(331, 199);
+            this.inputPanel.TabIndex = 2;
+            // 
+            // outputPanel
+            // 
+            
+                this.outputPanel.Location = new System.Drawing.Point(470, 68);
+                this.outputPanel.Name = "outputPanel";
+                this.outputPanel.Size = new System.Drawing.Size(318, 199);
+                this.outputPanel.TabIndex = 3;
+           
+            // 
+            // widthTrackBar
+            // 
+            this.widthTrackBar.Location = new System.Drawing.Point(15, 12);
+            this.widthTrackBar.Maximum = 6;
+            this.widthTrackBar.Minimum = 1;
+            this.widthTrackBar.Name = "widthTrackBar";
+            this.widthTrackBar.Size = new System.Drawing.Size(104, 56);
+            this.widthTrackBar.TabIndex = 4;
+            this.widthTrackBar.Value = 3;
+            this.widthTrackBar.Scroll += new System.EventHandler(this.WidthTrackBar_Scroll);
+            // 
+            // heightTrackBar
+            // 
+            this.heightTrackBar.Location = new System.Drawing.Point(150, 12);
+            this.heightTrackBar.Maximum = 6;
+            this.heightTrackBar.Minimum = 1;
+            this.heightTrackBar.Name = "heightTrackBar";
+            this.heightTrackBar.Size = new System.Drawing.Size(104, 56);
+            this.heightTrackBar.TabIndex = 5;
+            this.heightTrackBar.Value = 3;
+            this.heightTrackBar.Scroll += new System.EventHandler(this.HeightTrackBar_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 293);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(682, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Изменяем матрицу по правилу: Aij = bij/sum, если sum>0, Aij = -bij/sum, если sum<0, Aij = 0, если sum=0,\nгде sum - это сумма элементов i-той строки";
+            // 
+            // label2
+            // 
+    //        this.label2.AutoSize = true;
+    //        this.label2.Location = new System.Drawing.Point(101, 325);
+    //        this.label2.Name = "label2";
+    //        this.label2.Size = new System.Drawing.Size(584, 16);
+    //        this.label2.TabIndex = 7;
+    //        this.label2.Text = "Введена буква вместо цифры - обрабатываем как 0. Не ввели число - обрабатываем как " +
+    //"0.";
+            // 
+            // panelError
+            // 
+            this.panelError.Location = new System.Drawing.Point(84, 370);
+            this.panelError.Name = "panelError";
+            this.panelError.Size = new System.Drawing.Size(622, 41);
+            this.panelError.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelError);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputPanel);
+            this.Controls.Add(this.inputPanel);
+            this.Controls.Add(this.showMatrixButton);
+            this.Controls.Add(this.widthTrackBar);
+            this.Controls.Add(this.heightTrackBar);
+            this.Name = "Form1";
+            this.Text = "Lab2";
+            ((System.ComponentModel.ISupportInitialize)(this.widthTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Button showMatrixButton;
+        private System.Windows.Forms.Panel inputPanel;
+        private System.Windows.Forms.Panel outputPanel;
+        private System.Windows.Forms.TrackBar widthTrackBar;
+        private System.Windows.Forms.TrackBar heightTrackBar;
+        private System.Windows.Forms.Panel panelError;
+        private Label label1;
+        private Label label2;
+    }
+}
+
---
+++ 2/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Globalization; // Необходимо для использования CultureInfo 
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace lab2
+{
+
+    public class Pair<T1, T2>
+    {
+        public T1 First { get; set; }
+        public T2 Second { get; set; }
+        public Pair(T1 first, T2 second)
+        {
+            First = first;
+            Second = second;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        private TextBox[,] matrixTextBoxes;
+        private int rows = 3; // Количество строк
+        private int cols = 3; // Количество столбцов
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMatrixInputPanel(); // Инициализируем панели
+        }
+
+        private void WidthTrackBar_Scroll(object sender, EventArgs e)
+        {
+            cols = widthTrackBar.Value;
+            InitializeMatrixInputPanel(); // Обновить панель ввода
+        }
+
+        private void HeightTrackBar_Scroll(object sender, EventArgs e)
+        {
+            rows = heightTrackBar.Value;
+            InitializeMatrixInputPanel(); // Обновить панель ввода
+        }
+
+        private void InitializeMatrixInputPanel()
+        {
+            inputPanel.Controls.Clear(); // Очистить старые текстбоксы
+            matrixTextBoxes = new TextBox[rows, cols];
+            int textBoxWidth = 40;
+            int textBoxHeight = 20;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrixTextBoxes[i, j] = new TextBox
+                    {
+                        Width = textBoxWidth,
+                        Height = textBoxHeight,
+                        Location = new Point(j * textBoxWidth, i * textBoxHeight)
+                    };
+                    inputPanel.Controls.Add(matrixTextBoxes[i, j]);
+                }
+            }
+        }
+
+        private void showMatrixButton_Click(object sender, EventArgs e) // Обработчик для кнопки
+        {
+            ShowMatrix(outputPanel);
+        }
+
+        bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+
+            return true;
+        }
+        
+
+        private void ShowMatrix(Panel outputPanel)
+        {
+            panelError.ResetText();
+            List<Pair<int, int>> listOFErrors = new List<Pair<int, int>>();
+            List<Pair<double, bool>> listOfElements = new List<Pair<double, bool>>();
+
+            outputPanel.Controls.Clear(); // Очистить предыдущее содержимое
+
+            for (int i = 0; i < rows; i++)
+            {
+                listOfElements.Clear();
+                double sum = 0;
+                for (int j = 0; j < cols; j++)
+                {
+
+                    bool err = false;
+                    double aij;
+                    string input = matrixTextBoxes[i, j].Text.Trim();
+                    input = input.Replace(',', '.');
+
+                    CultureInfo culture = CultureInfo.InvariantCulture; 
+                    
+                    if (string.IsNullOrWhiteSpace(input))
+                    {
+                        err = true;
+                        aij = 0;
+                        listOFErrors.Add(new Pair<int, int>(i, j));
+                    }
+                    else if (!double.TryParse(input, NumberStyles.Any, culture, out aij))
+                        if (string.IsNullOrWhiteSpace(input))
+                        {
+                            err = true;
+                            aij = 0;
+                            listOFErrors.Add(new Pair<int, int>(i, j));
+                        }
+                        else if (!IsDigitsOnly(input))
+                        {
+                            err = true;
+                            aij = 0;
+                            listOFErrors.Add(new Pair<int, int>(i, j));
+                        }
+
+                    sum += aij;
+                    listOfElements.Add(new Pair<double, bool>(aij, err));
+                }
+                for (int j = 0; j < cols; j++) {
+                    Label label = new Label
+                    {
+                        Width = 40,
+                        Height = 20,
+                        Location = new Point(j * 40, i * 20)
+                    };
+                    if (sum > 0)
+                    { 
+                        label.Text = listOfElements[j].Second ? "Error" : (listOfElements[j].First /sum).ToString();
+                        
+                    }
+                    if (sum < 0)
+                    {
+                        label.Text = listOfElements[j].Second ? "Error" : (-listOfElements[j].First / sum).ToString();
+                    }
+                    if (sum == 0)
+                    {
+                        label.Text = listOfElements[j].Second ? "Error" : "0";
+                    }
+                    
+                    outputPanel.Controls.Add(label);
+                }
+            }
+
+
+            String message = "Ошибки допущены в элементах: ";
+            if (listOFErrors.Count == 0)
+            {
+                message = "Ошибок при заполнении нет.";
+            }
+
+            for (int i = 0; i < listOFErrors.Count; i++)
+            {
+                message += ("(" + (listOFErrors[i].First + ", " + listOFErrors[i].Second + "), "));
+            }
+
+            Label labelErr = new Label
+            {
+                Text = message.ToString(),
+                Width = 1000,
+                Height = 30,
+
+            };
+            panelError.Controls.Clear();
+            panelError.Controls.Add(labelErr);
+        }
+    }
+}
